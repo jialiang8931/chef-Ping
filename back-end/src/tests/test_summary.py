172dee3326b447de62bfda176874e1de658beac6
jjl <jialiang8455@gmail.com>
2023-01-25 15:59:12 +0800

    feat: add coutting with discount

--- back-end/src/tests/test_summary.py
+++ back-end/src/tests/test_summary.py
@@ -5,14 +5,29 @@ import unittest
 import utils.count_goods_prices as count_goods_prices
 
 class TestGoods(unittest.TestCase):
-    def testAddTwoGoodsPrice(self, ):
+    def setUp(self) -> None:
+        self.discount = 0
+        return super().setUp()
+
+    def testAddTwoGoodsPrices(self, ):
         testCaseInt = 100
-        totalPriceInt = count_goods_prices.countGoodsPrices(goodsPrices=testCaseInt, discount=0)
+        totalPriceInt = count_goods_prices.countGoodsPrices(goodsPrices=testCaseInt, discount=self.discount)
         self.assertEqual(100, totalPriceInt)
 
         testInts = [100, 100]
-        totalPriceInts = count_goods_prices.countGoodsPrices(testInts, discount=0)
+        totalPriceInts = count_goods_prices.countGoodsPrices(testInts, discount=self.discount)
         self.assertEqual(200, totalPriceInts)
+
+    def testAddTwoGoodsPricesWithDiscount(self, ):
+        self.discount = 0.5
+
+        testCaseInt = 100
+        totalPriceInt = count_goods_prices.countGoodsPrices(goodsPrices=testCaseInt, discount=self.discount)
+        self.assertEqual(50, totalPriceInt)
+
+        testInts = [100, 100]
+        totalPriceInts = count_goods_prices.countGoodsPrices(testInts, discount=self.discount)
+        self.assertEqual(100, totalPriceInts)
 
 
 if __name__ == "__main__":
--- back-end/src/utils/count_goods_prices.py
+++ back-end/src/utils/count_goods_prices.py
@@ -4,8 +4,8 @@ from functools import reduce
 def countGoodsPrices(goodsPrices: Union[int, List[int]], discount: Union[int, float] = 0) -> int:
     goodsPricesDiscount = 1 - discount 
     if type(goodsPrices) == int:
-        return goodsPrices * goodsPricesDiscount
+        return int(goodsPrices * goodsPricesDiscount)
     else:
         totalPriceOrigin = reduce(lambda acc, nextV: acc + nextV, goodsPrices, 0)
         totalPriceDiscounted = totalPriceOrigin * goodsPricesDiscount
-        return totalPriceDiscounted
+        return int(totalPriceDiscounted)
